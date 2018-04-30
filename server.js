20c5b47926dc9dce1d34776c2db576ddd1033b95
Darrell Abney <dabney@DoCS-1204.local>
2018-04-30 16:49:57 -0400

    getting stuck

--- server.js
+++ server.js
@@ -3,3 +3,23 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
+// create server with express
+var app = express();
+
+// set up port
+var PORT = process.env.PORT || 3000;
+
+// set up middleware (bodyParser)
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+// set up routes
+require('./routing/apiRoutes')(app);
+require('./routing/htmlRoutes')(app);
+
+// turn on server
+app.listen(PORT, function() {
+  console.log("you are now listening on port: " + PORT);
+});
